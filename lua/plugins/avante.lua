cbaaf4a2e92ce9721d87c6829ddd6aa01a4d2c96
Kylin <13345006231@163.com>
2025-02-10 14:46:44 +0800

    [plugin] support moonshot platform

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -5,13 +5,19 @@ return {
     lazy = false,
     version = false, -- set this if you want to always pull the latest change
     opts = {
-      provider = "deepseek",
+      provider = "moonshot",
       vendors = {
         deepseek = {
           __inherited_from = "openai",
           api_key_name = "DEEPSEEK_API_KEY",
           endpoint = "https://api.deepseek.com",
           model = "deepseek-coder",
+        },
+        moonshot = {
+          __inherited_from = "openai",
+          api_key_name = "MOONSHOT_API_KEY",
+          endpoint = "https://api.moonshot.cn/v1",
+          model = "moonshot-v1-auto",
         },
       },
     },
